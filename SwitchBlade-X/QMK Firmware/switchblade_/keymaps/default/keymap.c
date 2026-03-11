be37c95cd018532d3d0f70afe8d95c6071b93367
abhinavgupta-maker <abhinav53807@gmail.com>
2026-03-11 21:40:29 +0530

    Add files via upload

---
+++ SwitchBlade-X/QMK Firmware/switchblade_/keymaps/default/keymap.c
@@ -0,0 +1,13 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(
+        KC_KP_0, KC_KP_1, KC_KP_3, KC_KP_4,
+        KC_KP_5, KC_KP_6, KC_KP_7, KC_KP_8
+    )
+
+};
